081459da2e203966ab70fcfca92f1248e1f8ec96
Liang Guo <lguo@Liangs-MacBook-Pro.local>
2019-05-16 15:30:11 -0700

    Fix indentation in Order

--- src/css/src/main/java/com/css/kitchen/Order.java
+++ src/css/src/main/java/com/css/kitchen/Order.java
@@ -10,36 +10,36 @@ import lombok.Value;
 @Builder
 @Value
 public class Order {
-    public static final String ORDER_NAME = "name";
-    public static final String ORDER_TEMP = "temp";
-    public static final String ORDER_SHELFLIFE = "shelfLife";
-    public static final String ORDER_DECAYRATE = "decayRate";
+  public static final String ORDER_NAME = "name";
+  public static final String ORDER_TEMP = "temp";
+  public static final String ORDER_SHELFLIFE = "shelfLife";
+  public static final String ORDER_DECAYRATE = "decayRate";
 
-    private final String name;
-    private final Temperature temperature;
-    private final int shelfLife; // seconds
-    private final double decayRate;
+  private final String name;
+  private final Temperature temperature;
+  private final int shelfLife; // seconds
+  private final double decayRate;
 
-    public enum Temperature {
-        Hot,
-        Cold,
-        Frozen
-    }
+  public enum Temperature {
+    Hot,
+    Cold,
+    Frozen
+  }
 
-    // valid map of Temperature string value
-    static public ImmutableMap<String, Temperature> temperatureMap = ImmutableMap.of(
-            "hot", Temperature.Hot, "cold", Temperature.Cold, "frozen", Temperature.Frozen);
+  // valid map of Temperature string value
+  static public ImmutableMap<String, Temperature> temperatureMap = ImmutableMap.of(
+      "hot", Temperature.Hot, "cold", Temperature.Cold, "frozen", Temperature.Frozen);
 
-    public boolean isHot() { return temperature == Temperature.Hot; }
-    public boolean isCold() { return temperature == Temperature.Cold; }
-    public boolean isFrozen() { return temperature == Temperature.Frozen; }
+  public boolean isHot() { return temperature == Temperature.Hot; }
+  public boolean isCold() { return temperature == Temperature.Cold; }
+  public boolean isFrozen() { return temperature == Temperature.Frozen; }
 
-    public double getValue(int orderAge) {
-        return (double)(shelfLife - orderAge) - (decayRate * orderAge);
-    }
+  public double getValue(int orderAge) {
+    return (double)(shelfLife - orderAge) - (decayRate * orderAge);
+  }
 
-    public double getNormalizedValue(int orderAge) {
-        return getValue(orderAge) / (double)shelfLife;
-    }
+  public double getNormalizedValue(int orderAge) {
+    return getValue(orderAge) / (double)shelfLife;
+  }
 }
 
